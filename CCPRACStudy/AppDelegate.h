c18822419fd251fcb7e249f813361be1f0364e92
储诚鹏 <chuchengpeng0119@163.com>
2017-02-27 13:59:42 +0800

    Initial Commit

---
+++ CCPRACStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCPRACStudy
+//
+//  Created by 储诚鹏 on 17/2/27.
+//  Copyright © 2017年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
